74498269a97faa405606d9bf2d79cb0d82fe4319
black room <iniside.black.room@gmail.om>
2025-06-12 21:12:47 +0400

    update core model

--- fundinfo/core/models.py
+++ fundinfo/core/models.py
@@ -2,8 +2,18 @@ from django.db import models
 from fundinfo.common.models import BaseModel
 from django.utils.text import slugify
 import uuid
+from users.models import BaseUser
 
-class Product(BaseModel):
+class Base(BaseModel):
+    uuid = models.UUIDField(unique=True, default=uuid.uuid4)
+    deleted = models.BooleanField(default=False)
+    delete_data = models.DateTimeField(default=None, null=True, blank=True)
+    user = models.ForeignKey(BaseUser, on_delete=models.PROTECT)
+    
+    class Meta:
+        abstract = True
+
+class Product(Base):
     # STATUS_ENABLED = 0
     # STATUS_DISABLED = 1
     # STATUS_DELETED = 2
@@ -13,21 +23,19 @@ class Product(BaseModel):
     #         (STATUS_DELETED, "Deleted")
     #         )
     name = models.CharField(max_length=255)
-    uuid = models.UUIDField(unique=True, default=uuid.uuid4)
     slug = models.SlugField()
     description = models.TextField()
     price = models.IntegerField(default=0)
     discount = models.FloatField(default=0)
     enabled = models.BooleanField(default=True)
-    deleted = models.BooleanField(default=False)
     # status = models.IntegerField(max_length=10, choices=STATUS_CHOICES, default=STATUS_ENABLED) 
     category = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='products')
     old_category = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='old_producs')
 
-class Category(BaseModel):
+class Category(Base):
     name = models.CharField(max_length=255)
-    uuid = models.UUIDField(unique=True, default=uuid.uuid4)
     # product_set => products
     # old_products
     parent = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, default=None, related_name='childs')
+
 
