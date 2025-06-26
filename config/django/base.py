5de5f120c90216c48afe69c2e66447f1df5c63b2
black room <iniside.black.room@gmail.om>
2025-06-26 15:08:03 +0330

    config sending main settings

--- config/django/base.py
+++ config/django/base.py
@@ -180,4 +180,4 @@ from config.settings.sessions import *  # noqa
 from config.settings.celery import *  # noqa
 from config.settings.swagger import *  # noqa
 #from config.settings.sentry import *  # noqa
-#from config.settings.email_sending import *  # noqa
+from config.settings.email_sending import *  # noqa
